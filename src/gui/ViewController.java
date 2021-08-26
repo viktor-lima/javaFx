46cfbe0d65dd85396a0704308b015cfdd354a036
viktor-lima <joaolimak10@outlook.com>
2021-08-25 22:10:56 -0300

    javafx3

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,7 +1,9 @@
 package gui;
 
+import gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Alert.AlertType;
 
 public class ViewController {
 
@@ -10,7 +12,7 @@ public class ViewController {
 
 	@FXML
 	public void onBtTestAction() {
-		System.out.println("Click");
+		Alerts.showAlert("Alert title", null, "Hello", AlertType.ERROR);
 	}
 	
 }
